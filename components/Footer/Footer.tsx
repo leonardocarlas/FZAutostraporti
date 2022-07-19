1cd2476de66717e682c00c0e420eb703f4c72a50
Leonardo Carlassare <lio.del.bronx@gmail.com>
2022-07-19 22:26:17 +0200

    Basic Footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -18,21 +18,36 @@ export default function Footer () {
   return (
     <footer className={`p-2 bg-black text-white link-white ${styles.link}`} data-testid="container">
       <div className={'flex flex-row flex-wrap justify-around'}>
-        <div><p>LOGO</p></div>
+        <div>
+          <p className={utilStyles.subtitle}>ORARI LAVORATIVI</p>
+          <p>Lunedì______8:00-17:00</p>
+          <p>Martedì</p>
+          <p>Mercoledì</p>
+          <p>Giovedì</p>
+          <p>Venerdì</p>
+          <p>Sabato e Domenica</p>
+        </div>
         <div className={'flex flex-col'}>
-          <p>Menu:</p>
+          <p className={utilStyles.subtitle}>MENU</p>
           <Link href={'#'} ><a className={`${utilStyles.link} ${styles.link}`}>{t.header.home}</a></Link>
           <Link href={PathPage.SERVICES} ><a className={`${utilStyles.link} ${styles.link}`}>{t.header.services}</a></Link>
           <Link href={'#'} ><a className={`${utilStyles.link} ${styles.link}`}>{t.header.aboutUs}</a></Link>
           <Link href={'#'} ><a className={`${utilStyles.link} ${styles.link}`}>{t.header.buttonContactUs}</a></Link>
         </div>
         <div className={'flex flex-col'}>
-          <p>Contatti:</p>
-          <p>info@fzautotrasporti.it</p>
-          <p>Via Ospedale 7, Santorso</p>
+          <p className={utilStyles.subtitle}>SEDE OPERATIVA</p>
+          <p>Via Garziere 13, Santorso</p>
+          <p>36014</p>
+          <p>Vicenza, Italia</p>
+        </div>
+
+        <div className={'flex flex-col'}>
+          <p className={utilStyles.subtitle}>CONTATTI</p>
+          <p>Email: info@fzautotrasporti.it</p>
+          <p>Cellulare: </p>
         </div>
       </div>
-      <div className={'flex flex-row justify-center'}>
+      <div className={'flex flex-row justify-center text-2xl'}>
         <p>Website developed by <Link href={'https://www.leonardocarlassare.com/'}><a>Leonardo Carlassare</a></Link></p>
       </div>
     </footer>
